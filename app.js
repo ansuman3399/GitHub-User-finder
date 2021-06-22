634af0e66277f88e5276e00c5eb6d4d6bb0ae9d0
Ansuman Mohanty <ansumanm20@gmail.com>
2021-06-22 19:27:12 +0530

    Added alert message and repos display

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const searchUser = document.getElementById("searchUser");
 searchUser.addEventListener("keyup", (event) => {
   const user = event.target.value;
   if (user !== "") {
-    github
-      .getuser(user)
-      .then((data) => {
-        if (data.profile.message === "Not Found") {
-          console.log("Inside if");
-        } else {
-          console.log(data);
-          ui.showProfile(data.profile);
-        }
-      })
-      .catch((err) => console.log(err));
+    github.getuser(user).then((data) => {
+      if (data.profile.message === "Not Found") {
+        console.log("Inside if");
+        ui.showAlert("User not found", "alert alert-danger");
+      } else {
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    ui.clearProfile();
   }
 });
--- githubSearch.js
+++ githubSearch.js
@@ -2,15 +2,24 @@ class Github {
   constructor() {
     this.clientId = "bca822121ce1b094aca3";
     this.clientSecret = "a0efc712aef2e996d999412375970009643ec1ab";
+    this.reposCount = 5;
+    this.reposSort = "created : asc";
   }
 
   async getuser(user) {
     const profileResponse = await fetch(
       `https://api.github.com/users/${user}?clientId=${this.clientId}&clientSecret=${this.clientSecret}`
     );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.reposCount}&sort=${this.reposSort}&clientId=${this.clientId}&clientSecret=${this.clientSecret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
     return {
       profile: profile,
+      repos,
     };
   }
 }
